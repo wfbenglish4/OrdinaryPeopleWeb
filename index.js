e81e8bba9bd91b5e8c3c2534647527cfe891f903
wfbenglish4 <alnaser.alfawakhiri@students.wfbschools.com>
2016-05-23 21:34:14 -0500

    Add files

---
+++ index.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="jquery-2.2.3.js" />
+/// <reference path="jquery-ui-1.11.3.js" />
+$(document).ready(function () {
+    $('.active-slide #menu').accordion({
+        collapsible: true
+    });
+    $('.active-slide #menu').accordion("option", "active", false);
+    $('.arrow-next').click(function () {
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+
+        if (nextSlide.length ===0) {
+            nextSlide = $('.first');
+            nextDot = $('.first-dot');
+        }
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass('active-slide');
+        nextSlide.fadeIn(600);
+        nextSlide.addClass('active-slide');
+
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+        $('.active-slide #menu').accordion({
+            collapsible: true
+        });
+        $('.active-slide #menu').accordion("option", "active", false);
+    });
+
+
+    $('.arrow-prev').click(function () {
+        var currentSlide = $('.active-slide');
+        var prevSlide = currentSlide.prev();
+
+        var currentDot = $('.active-dot');
+        var prevDot = currentDot.prev();
+
+        if (prevSlide.length === 0) {
+            prevSlide = $('.slide').last();
+            prevDot = $('.dot').last();
+        }
+
+        currentSlide.fadeOut(600);
+        currentSlide.removeClass('active-slide');
+        prevSlide.fadeIn(600);
+        prevSlide.addClass('active-slide');
+
+        currentDot.removeClass('active-dot');
+        prevDot.addClass('active-dot');
+        $('.active-slide #menu').accordion({
+            collapsible: true
+        });
+        $('.active-slide #menu').accordion("option", "active", false);
+    });
+})
---
+++ main_controller.js
@@ -0,0 +1,3 @@
+﻿/// <reference path="jquery-2.2.3.js" />
+/// <reference path="jquery-ui-1.11.3.js" />
+//Done! Reference paths hopefully done!!!!!
